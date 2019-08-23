bb4f7dc865a766e288b22bc00271672e4d4061cf
wangxirui <372137966qq>
2019-08-23 10:15:34 +0800

    iuiouoi

---
+++ src/main/java/com/qf/pojo/Permission.java
@@ -0,0 +1,17 @@
+package com.qf.pojo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Auther: 王玺瑞
+ * @Date: 2019/8/23 10:10
+ * @Description:
+ */
+@Data
+public class Permission implements Serializable {
+    private int perId;
+    private int pId;
+    private String perName;
+}
